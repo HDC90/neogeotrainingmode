0d4725224a0e7dcbbac57564ea37dde98b49e261
michael da costa <michaeldgdc@gmail.com>
2018-12-28 17:07:49 +0100

    P1 to P2 fonctionnel + bouton on/off fonctionnel

--- old/p1top2v2.py
+++ old/p1top2v2.py
@@ -9,25 +9,36 @@ def p1top2():
         for event in events:
             print(event.ev_type, event.code, event.state)
             # direction vers le bas
-            if event.code == 'ABS_HAT0Y' and event.state == 1:
+            if event.code == 'ABS_HAT0Y' and event.state == 0:
                 pyautogui.keyDown('s')
-            else:
+                pyautogui.press('s')
                 pyautogui.keyUp('s')
             # direction vers le haut
             if event.code == 'ABS_HAT0Y' and event.state == int(-1):
                 pyautogui.keyDown('z')
-            else:
                 pyautogui.keyUp('z')
             # direction vers la gauche
             if event.code == 'ABS_HAT0X' and event.state == int(-1):
                 pyautogui.keyDown('q')
-            else:
                 pyautogui.keyUp('q')
             # direction vers la droite
             if event.code == 'ABS_HAT0X' and event.state == int(1):
                 pyautogui.keyDown('d')
-            else:
                 pyautogui.keyUp('d')
-
-
+            # bouton A
+            if event.code == 'BTN_WEST' and event.state == int(1):
+                pyautogui.keyDown('i')
+                pyautogui.keyUp('i')
+            # bouton B
+            if event.code == 'BTN_SOUTH' and event.state == int(1):
+                pyautogui.keyDown('k')
+                pyautogui.keyUp('k')
+            # bouton A
+            if event.code == 'BTN_NORTH' and event.state == int(1):
+                pyautogui.keyDown('o')
+                pyautogui.keyUp('o')
+            # bouton A
+            if event.code == 'BTN_EAST' and event.state == int(1):
+                pyautogui.keyDown('l')
+                pyautogui.keyUp('l')
 p1top2()
